afa0276db0821f2ebcc16a75f4a5ccb56707fb7d
UX425 <UX425@10.20.35.191>
2025-10-30 12:48:02 +0100

    comprobar

--- ProyectoCampingParty/src/main/java/VISTA/CLienteLogLogin.java
+++ ProyectoCampingParty/src/main/java/VISTA/CLienteLogLogin.java
@@ -1,3 +1,4 @@
+/*hola*/
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
